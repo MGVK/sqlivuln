32bb65244733ecd783de7ad0018d9d2fbcbdb806
MGVK <mukylike@yandex.ru>
2021-06-03 19:53:18 +0300

    with auth

--- src/main/java/ru/mgvk/sqlivuln/SqlivulnApplication.java
+++ src/main/java/ru/mgvk/sqlivuln/SqlivulnApplication.java
@@ -1,13 +1,51 @@
 package ru.mgvk.sqlivuln;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @SpringBootApplication
 public class SqlivulnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SqlivulnApplication.class, args);
+    }
+
+
+    @Configuration
+    @EnableWebSecurity
+    public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("user1")
+                    .password(passwordEncoder().encode("123456"))
+                    .roles("ADMIN");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
     }
 
 }
